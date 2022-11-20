f2c21d4e9f0f663a781a847c81450bfb2e55e75d
sybao <luongsybao2505@gmail.com>
2022-11-20 23:47:45 +0700

    fix css

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,12 +7,35 @@ export class Result extends Component {
     return (
       <div className="pt-3">
         <h3 className="text-light fw-bold">DANH SÁCH GHẾ BẠN CHỌN</h3>
+        <div className="mt-3">
+          <div>
+            <button
+              style={{ cursor: "initial" }}
+              className="btn p-2 bg-danger me-2"
+            ></button>
+            <span className="fw-semibold text-danger">Ghế đã chọn</span>
+          </div>
+          <div>
+            <button
+              style={{ cursor: "initial" }}
+              className="btn p-2 bg-warning me-2"
+            ></button>
+            <span className="fw-semibold text-warning">Ghế đang chọn</span>
+          </div>
+          <div>
+            <button
+              style={{ cursor: "initial" }}
+              className="btn p-2 bg-light me-2"
+            ></button>
+            <span className="fw-semibold text-light">Ghế chưa chọn</span>
+          </div>
+        </div>
         <div>
-          <table className="table">
+          <table className="table mt-3">
             <thead>
               <tr>
-                <td className="text-light">Số ghế</td>
-                <td className="text-light">Giá tiền</td>
+                <td className="text-light fw-bold">Số ghế</td>
+                <td className="text-light fw-bold">Giá tiền</td>
                 <td></td>
               </tr>
             </thead>
@@ -20,18 +43,18 @@ export class Result extends Component {
               {productsSelectedList.map((item) => {
                 return (
                   <tr key={item.soGhe}>
-                    <td className="text-light ">{item.soGhe}</td>
-                    <td className="text-light ">
+                    <td className="text-warning ">{item.soGhe}</td>
+                    <td className="text-warning ">
                       {item.gia.toLocaleString()} VND
                     </td>
                     <td>
                       <button
-                        className="btn btn-danger"
+                        className="btn btn-danger fw-bold"
                         onClick={() => {
                           handleDelete(item);
                         }}
                       >
-                        Hủy
+                        X
                       </button>
                     </td>
                   </tr>
